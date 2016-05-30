63dbff30f72d850de67ae92c4a981aa1d5b1ca3a
Joe White <white.joe4@gmail.com>
2016-05-30 16:52:47 +0100

    initial commit

---
+++ Assets/GloopController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GloopController : MonoBehaviour {
+
+	public WandController wandLeft;
+    public WandController wandRight;
+    private Hv_bubbles_LibWrapper _bubbles;
+
+	void Start() {
+        // OSCHandler.Instance.Init();
+        _bubbles = GetComponent<Hv_bubbles_LibWrapper>();
+	}
+
+	void FixedUpdate() {
+        // OSCHandler.Instance.SendMessageToClient("Pd", "/wand/left/accel", wandLeft.acceleration);
+        _bubbles.accel = wandLeft.acceleration;
+	}
+
+    public void OnTriggerStart() {
+        // OSCHandler.Instance.SendMessageToClient("Pd", "/wand/left/trigger", 1);
+        _bubbles.trigger = 1.0f;
+    }
+
+    public void OnTriggerEnd() {
+        // OSCHandler.Instance.SendMessageToClient("Pd", "/wand/left/trigger", 0);
+        _bubbles.trigger = 0.0f;
+    }
+}
---
+++ Assets/WandController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+
+    public UnityEvent OnTriggerStart;
+    public UnityEvent OnTriggerEnd;
+
+    public float acceleration = 0.0f;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+    private Vector3 prevPosition;
+
+	void Start () {
+	   trackedObject = GetComponent<SteamVR_TrackedObject>();
+       prevPosition = trackedObject.transform.position;
+	}
+
+	void Update () {
+        if (controller != null) {
+         //   triggerButtonPressed = controller.GetPress(triggerButton);
+
+            if (controller.GetPressDown(triggerButton)) OnTriggerStart.Invoke();
+            if (controller.GetPressUp(triggerButton)) OnTriggerEnd.Invoke();
+        }
+	}
+
+    void FixedUpdate() {
+        Vector3 accel = trackedObject.transform.position - prevPosition;
+        acceleration = accel.magnitude;
+        prevPosition = trackedObject.transform.position;
+    }
+}
